a0e704d3366aa2add0ec312568be83f7036cf116
LrBrb <LrBrb@LAPTOPLRBRBT>
2023-10-30 15:00:47 +0300

    Добавьте файлы проекта.

---
+++ project1_SDT/Matrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1_SDT
+{
+    public static class Matrix
+    {
+        public static double[,] CreateMatrix(int rowCount, int columnCount)
+        {
+            return new double[rowCount, columnCount];
+        }
+        public static double[,] CreateMatrix(int size)
+        {
+            return new double[size, size];
+        }
+        public static void FillMatrix(double[,] matrix)
+        {
+            Random rnd = new();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = Math.Round(rnd.NextDouble(), 2);
+                }
+            }
+        }
+
+        public static void ShowMatrix(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static double[,] TranspanentMatrix(double[,] matrix)
+        {
+            double[,] resultMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    resultMatrix[i,j] = matrix[j,i];    
+                }
+            }
+            return resultMatrix;
+
+        }
+    }
+}
---
+++ project1_SDT/Program.cs
@@ -0,0 +1,81 @@
+﻿using project1_SDT;
+
+Console.WriteLine("Практическая работа №1");
+Thread.Sleep(1000);
+
+Console.WriteLine();
+Console.WriteLine("Транспонирование квадратной матрицы");
+
+Thread.Sleep(1000); 
+Console.WriteLine();
+
+Console.Write("Введите размер квадратной матрицы: ");
+
+int sizeMatrix = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Создание и заполнение матрицы...");
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine("...");
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Исходная матрица: ");
+
+Console.WriteLine();
+
+double[,] matrix = Matrix.CreateMatrix(sizeMatrix);
+Matrix.FillMatrix(matrix);
+
+Matrix.ShowMatrix(matrix);
+
+Console.WriteLine();
+
+link1:
+
+Console.WriteLine("Транспонировать матрицу? (да/нет)");
+Console.WriteLine();
+
+string userResponse = Console.ReadLine();
+userResponse = userResponse.ToLower();
+
+if(userResponse == "да")
+{
+    Console.WriteLine();
+    Console.WriteLine("Транспонирование матрицы...");
+
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine("...");
+
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine("нет");
+
+    Thread.Sleep(10000);
+
+    Console.WriteLine();
+
+    Console.WriteLine("Транспонированная матрица: ");
+
+    matrix = Matrix.TranspanentMatrix(matrix);
+
+    Console.WriteLine();
+
+    Matrix.ShowMatrix(matrix);
+
+    Console.WriteLine();
+
+}
+else
+{
+    Console.WriteLine("НЕПРАВИЛЬНЫЙ ОТВЕТ!!!!");
+    Console.WriteLine();
+    goto link1;
+}
